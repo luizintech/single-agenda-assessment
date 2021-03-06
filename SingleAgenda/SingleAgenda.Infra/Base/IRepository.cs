b954320acdc7e7e5e29b5b32876dc3fe4187f181
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2021-03-06 16:28:42 -0300

    made the conventions for the repository and crud methods

---
+++ SingleAgenda/SingleAgenda.Infra/Base/IRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace SingleAgenda.Infra.Base
+{
+    public interface IRepository<T> where 
+        T : class
+    {
+
+        /// <summary>
+        /// Will list all row, except the removed.
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<T> List();
+
+        /// <summary>
+        /// Will list all row, including the removed.
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<T> GetAll();
+
+        /// <summary>
+        /// For some filter conditions, pass the predicate.
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<T> Get(Expression<Func<T, bool>> predicate);
+
+        T GetById(int id);
+
+        int Add(T entity);
+        void Update(T entity);
+
+        void Delete(int id);
+
+    }
+}
---
+++ SingleAgenda/SingleAgenda.Infra/Base/RepositoryBase.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SingleAgenda.Entities.Base;
+using SingleAgenda.Infra.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace SingleAgenda.Infra.Base
+{
+    public abstract class RepositoryBase<T>
+        : IRepository<T>, IDisposable
+        where T : EntityBase
+    {
+
+        #region Constructors
+
+        private readonly SingleAgendaDbContext _context;
+
+        protected RepositoryBase(IServiceProvider serviceProvider)
+            : this(new SingleAgendaDbContext(serviceProvider))
+        {
+        }
+        
+        public RepositoryBase(SingleAgendaDbContext context)
+        {
+            _context = context;
+        }
+
+        #endregion
+
+        public int Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            return _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var entity = this.GetById(id);
+            entity.UpdatedAt = DateTime.Now;
+            entity.Removed = true;
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.Set<T>().Update(entity);
+        }
+
+        public IEnumerable<T> Get(Expression<Func<T, bool>> predicate)
+        {
+            return _context.Set<T>().Where(predicate).AsEnumerable<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>()
+                .Where(et => et.Removed == true)
+                .ToArray();
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>()
+                .Where(et => et.Id == id)
+                .SingleOrDefault();
+        }
+
+        public IEnumerable<T> List()
+        {
+            return _context.Set<T>()
+                .Where(et => et.Removed == false)
+                .ToArray();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            // Todo the disposable method according.
+        }
+    }
+}
